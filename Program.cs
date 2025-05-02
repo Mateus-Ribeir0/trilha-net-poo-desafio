fc88cd4b6829acd95f61580c10e288adb1009cc5
Mateus Ribeiro <ribeiro211097@gmail.com>
2025-05-02 16:15:06 -0300

    Finaliza desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Nokia:");
+        Smartphone nokia = new Nokia(numero: "1111111", modelo: "Nokia", imei: "111111111", memoria: 128);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Xp investimentos");
+
+        Console.WriteLine("\niPhone:");
+        Smartphone iphone = new Iphone(numero: "1111111", modelo: "iPhone 15", imei: "11111111", memoria: 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Xp investimentos");
+    }
+}
